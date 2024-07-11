76b6bb48c69dcd150f6913d9e4dc55dacfdcd77c
Vidya-account <vidya18Y@gmail.com>
2024-07-11 17:38:07 +0530

    Successfully added all files into 1st repository

---
+++ Input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Input
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String name=s.next();
+System.out.println("your name is as "+name);
+}
+}
---
+++ PositiveOrNegative.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class PositiveOrNegative
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n1=s.nextInt();
+if(n1>0)
+System.out.println(n1+" it is positive");
+else
+System.out.println(n1+" it is positive");
+}
+}
